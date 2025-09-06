f4d5bd94ed6756a8f9ec62f8497cc62855c27087
Denis Ganeev <145168530+Keeepoo@users.noreply.github.com>
2025-09-07 00:24:29 +0300

    uppdate homework-1 - fixed errors

--- homework-1/src/app/App.tsx
+++ homework-1/src/app/App.tsx
@@ -1,18 +1,14 @@
 import MainLayout from "../shared/layouts/MainLayout";
 import PostList from "../widgets/PostList/PostList";
-import styles from './App.module.css';
+import Title from "../widgets/Title/Title.post";
+import { posts } from "../lib/mocks/posts";
+import styles from "./App.module.css";
 
 function App() {
-  const posts = [
-    { id: 1, title: "Денис Ганеев", text: "Студент ASTON" },
-    { id: 1, title: "Татьяна Фомичева", text: "Студент ASTON" },
-    { id: 1, title: "Олег Петров", text: "Студент ASTON" }
-  ];
-
   return (
     <div className={styles.app}>
       <MainLayout>
-        <h1 className={styles.title}>Список постов</h1>
+        <Title></Title>
         <PostList posts={posts} />
       </MainLayout>
     </div>
